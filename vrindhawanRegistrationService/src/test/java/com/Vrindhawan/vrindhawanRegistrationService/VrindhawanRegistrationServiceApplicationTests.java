7c9d2e17931714af08c55b446b3b9b021db15b9a
Nikhil <nikhilmasurkar1997@gmail.com>
2022-03-16 20:02:07 +0530

    this is my first project and work under process

---
+++ vrindhawanRegistrationService/src/test/java/com/Vrindhawan/vrindhawanRegistrationService/VrindhawanRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vrindhawan.vrindhawanRegistrationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrindhawanRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
