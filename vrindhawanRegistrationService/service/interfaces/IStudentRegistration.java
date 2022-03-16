3abe29853015440cbde852ce3b6da8e67242d9ae
Nikhil <nikhilmasurkar1997@gmail.com>
2022-03-16 19:40:10 +0530

    this is my first project and incomplete

---
+++ vrindhawanRegistrationService/service/interfaces/IStudentRegistration.java
@@ -0,0 +1,7 @@
+package com.Vrindhawan.vrindhawanRegistrationService.service.interfaces;
+
+import com.Vrindhawan.vrindhawanRegistrationService.model.Student;
+
+public interface IStudentRegistration {
+    public Student createStudent (Student student);
+}
