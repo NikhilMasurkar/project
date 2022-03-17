3f4188dc0fa92ca0938149eb5029dd3f0b98dd00
Nikhil <nikhilmasurkar1997@gmail.com>
2022-03-17 14:33:11 +0530

    Initial stage

--- src/main/java/com/Vrindhawan/vrindhawanRegistrationService/service/interfaces/IStudentRegistration.java
+++
@@ -1,7 +0,0 @@
-package com.Vrindhawan.vrindhawanRegistrationService.service.interfaces;
-
-import com.Vrindhawan.vrindhawanRegistrationService.model.Student;
-
-public interface IStudentRegistration {
-    public Student createStudent (Student student);
-}
